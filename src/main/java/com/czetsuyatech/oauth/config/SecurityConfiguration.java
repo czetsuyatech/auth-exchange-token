470c040cc923549fa499c9d1dabb1983aced1d63
Edward P. Legaspi <czetsuya@gmail.com>
2023-04-27 09:16:36 +0800

    chore: login to Google with Keycloak

--- src/main/java/com/czetsuyatech/oauth/config/SecurityConfiguration.java
+++ src/main/java/com/czetsuyatech/oauth/config/SecurityConfiguration.java
@@ -7,12 +7,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
+
+  @Bean
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,10 +37,4 @@ public class SecurityConfiguration {
     ;
     return http.build();
   }
-
-//  @Bean
-//  public OpaqueTokenIntrospector introspector() {
-//
-//    return new GoogleTokenIntrospector("https://oauth2.googleapis.com/tokeninfo");
-//  }
 }
