e2668075c6e1a7baa3911084dd78bbbfcf3d5e91
Edward P. Legaspi <czetsuya@gmail.com>
2023-04-27 09:30:57 +0800

    chore: inject principal in controller

--- src/main/java/com/czetsuyatech/oauth/web/controllers/HelloController.java
+++ src/main/java/com/czetsuyatech/oauth/web/controllers/HelloController.java
@@ -2,9 +2,12 @@ package com.czetsuyatech.oauth.web.controllers;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,7 @@ public class HelloController {
 
   @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
   @ResponseStatus(OK)
-  public ResponseEntity<String> sayHello() {
+  public ResponseEntity<String> sayHello(Authentication principal) {
     return ResponseEntity.ok("Hello World");
   }
 }
