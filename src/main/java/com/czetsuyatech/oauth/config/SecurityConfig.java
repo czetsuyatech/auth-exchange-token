e5e327674414bfeab9be4221e299390164ada8b0
Edward P. Legaspi <czetsuya@gmail.com>
2023-05-17 20:37:02 +0800

    chore: updated realm

--- src/main/java/com/czetsuyatech/oauth/config/SecurityConfig.java
+++ src/main/java/com/czetsuyatech/oauth/config/SecurityConfig.java
@@ -7,6 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -17,29 +23,48 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+  @Bean
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(e -> e.disable())
-                .cors()
-                .and()
-                .authorizeHttpRequests(e -> e.anyRequest().authenticated())
+    http.csrf(e -> e.disable())
+        .cors()
+        .and()
+        .authorizeHttpRequests(e -> e.anyRequest().authenticated())
 
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .sessionAuthenticationStrategy(new NullAuthenticatedSessionStrategy())
 
-                .and()
-                .httpBasic(e -> e.disable())
-                .formLogin(e -> e.disable())
-                .oauth2ResourceServer().jwt()
-        ;
+        .and()
+        .httpBasic(e -> e.disable())
+        .formLogin(e -> e.disable())
+        .oauth2ResourceServer().jwt()
+    ;
 
-        return http.build();
-    }
+    return http.build();
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      final ClientRegistrationRepository clientRegistrationRepository,
+      final OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+    final OAuth2AuthorizedClientProvider authorizedClientProvider =
+        OAuth2AuthorizedClientProviderBuilder.builder()
+            .authorizationCode()
+            .refreshToken()
+            .clientCredentials()
+            .password()
+            .build();
+
+    final DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+        clientRegistrationRepository, authorizedClientRepository);
+    authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+    return authorizedClientManager;
+  }
 }
--- src/main/java/com/czetsuyatech/oauth/web/controllers/CalendarController.java
+++ src/main/java/com/czetsuyatech/oauth/web/controllers/CalendarController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CalendarController {
 
-  @GetMapping("/calendar-entries")
+  private final OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+  @GetMapping("/calendars/entries")
   public String entries() {
 
     log.debug("token={}", getSessionBearerToken());
 
-
     return "Hello";
-
   }
   private String getSessionBearerToken() {
 
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     JwtAuthenticationToken oauthToken = (JwtAuthenticationToken) authentication;
+    String accessToken = oauthToken.getToken().getTokenValue();
+
     return oauthToken.getToken().getTokenValue();
 //    OAuth2AuthorizedClient client =
 //        authorizedClientService.loadAuthorizedClient(
