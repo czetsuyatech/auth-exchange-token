35f7f68fd62882ef4185aef60cebffa5b31b96c2
Edward P. Legaspi <czetsuya@gmail.com>
2023-05-19 20:15:59 +0800

    feat: add Gmail access

--- src/main/java/com/czetsuyatech/oauth/clients/ClientFactory.java
+++ src/main/java/com/czetsuyatech/oauth/clients/ClientFactory.java
@@ -1,6 +1,7 @@
 package com.czetsuyatech.oauth.clients;
 
 import com.czetsuyatech.oauth.clients.google.GoogleCalendarClient;
+import com.czetsuyatech.oauth.clients.google.GoogleEmailClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -8,19 +9,28 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class ClientFactory {
 
   private static final String GOOGLE_CALENDAR_V3_API = "https://www.googleapis.com/calendar/v3";
+  private static final String GOOGLE_GMAIL_V3_API = "https://gmail.googleapis.com/gmail/v1";
 
   public static GoogleCalendarClient googleCalendarClient(String googleToken) {
 
-    HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter
-            .forClient(googleCalendarWebClient(googleToken)))
-        .build();
-
-    return factory.createClient(GoogleCalendarClient.class);
+    return getFactory(GOOGLE_CALENDAR_V3_API, googleToken).createClient(GoogleCalendarClient.class);
   }
 
-  private static WebClient googleCalendarWebClient(String googleToken) {
+  public static GoogleEmailClient googleEmailClient(String googleToken) {
+
+    return getFactory(GOOGLE_GMAIL_V3_API, googleToken).createClient(GoogleEmailClient.class);
+  }
+
+  private static HttpServiceProxyFactory getFactory(String baseUrl, String googleToken) {
+
+    return HttpServiceProxyFactory.builder(
+            WebClientAdapter.forClient(googleCalendarWebClient(baseUrl, googleToken)))
+        .build();
+  }
+
+  private static WebClient googleCalendarWebClient(String baseUrl, String googleToken) {
     return WebClient.builder()
-        .baseUrl(GOOGLE_CALENDAR_V3_API)
+        .baseUrl(baseUrl)
         .defaultHeaders(httpHeaders -> {
           httpHeaders.setBearerAuth(googleToken);
         })
